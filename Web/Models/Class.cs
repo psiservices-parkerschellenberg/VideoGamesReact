9086198a94cad197a5abb5cb9a67374ff8fff4e9
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-23 13:54:08 -0500

    Add Game model

---
+++ Web/Models/Class.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+//using System.Xml.Linq;
+
+namespace Web.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Developer { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
