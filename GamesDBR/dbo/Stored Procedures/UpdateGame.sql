86b846e1f6c83fce1945e73b6a1092be1ee03033
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-23 14:47:08 -0500

    Add UpdateGame stored procedure

---
+++ GamesDBR/dbo/Stored Procedures/UpdateGame.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[UpdateGame]
+    @Id INT,
+    @Title VARCHAR(50),
+    @ReleaseDate DATE,
+    @Developer VARCHAR(50),
+    @Price DECIMAL(18, 2)
+AS
+BEGIN
+    UPDATE dbo.GamesNew
+    SET Title = @Title,
+        ReleaseDate = @ReleaseDate,
+        Developer = @Developer,
+        Price = @Price
+    WHERE Id = @Id
+END
