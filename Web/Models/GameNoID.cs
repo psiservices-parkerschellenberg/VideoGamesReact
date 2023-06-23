93ec3a98a8ebd7620da682fa9e6a396437d937a6
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-23 14:03:59 -0500

    Add GameNoID model

---
+++ Web/Models/GameNoID.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web.Models
+{
+    public class GameNoID
+    {
+        public string? Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Developer { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
