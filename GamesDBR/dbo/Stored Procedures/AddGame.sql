8150ea1e94942307c6b93b0d39861b804be174a6
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-23 14:46:17 -0500

    Add AddGame stored procedure

---
+++ GamesDBR/dbo/Stored Procedures/AddGame.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[AddGame]
+    @Title varchar(50),
+	@ReleaseDate date,
+    @Developer varchar(50) = NULL,
+    @Price decimal(18, 2)
+AS
+BEGIN
+    INSERT INTO GamesR(Title, ReleaseDate, Developer, Price)
+    VALUES (@Title, @ReleaseDate, @Developer, @Price)
+END
