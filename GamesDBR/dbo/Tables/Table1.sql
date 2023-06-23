1be6b20fdb07d20896bdf36b68c91e496f40d75f
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-23 14:09:04 -0500

    Add GamesR table

---
+++ GamesDBR/dbo/Tables/Table1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[GamesR]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [Title] VARCHAR(50) NOT NULL, 
+    [ReleaseDate] DATE NOT NULL, 
+    [Developer] VARCHAR(50) NULL, 
+    [Price] DECIMAL(18, 2) NOT NULL
+)
