223fa84247d8cef1f0f0bf77f16a6b2ede7ce9b1
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-26 11:34:19 -0500

    Install bootstrap npm

--- Web/ClientApp/src/index.js
+++ Web/ClientApp/src/index.js
@@ -1,17 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={baseUrl}>
     <App />
-  </BrowserRouter>);
-
+);
