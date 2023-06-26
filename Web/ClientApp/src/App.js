a16749a4940e2e69d3213cfe8dacfb42524ea050
Parker Schellenberg <parker.schellenberg@psionline.com>
2023-06-26 11:39:44 -0500

    Edit App js

--- Web/ClientApp/src/App.js
+++ Web/ClientApp/src/App.js
@@ -1,22 +1,12 @@
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
-import './custom.css';
+//import logo from './logo.svg';
+import './App.css';
+import Layout from './components/shared/Layout';
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render() {
+function App() {
     return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
+        <Layout>
+            <h1>Hello World</h1>
+        </Layout>
     );
-  }
 }
+export default App;
